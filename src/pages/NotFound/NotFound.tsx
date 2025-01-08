2bb0a32cac41c92cecc237a4283c0a418979ecc7
Nataly Suvorova <suvorova.nata.a@gmail.com>
2025-01-08 16:09:30 +0300

    branch deploy

--- src/pages/NotFound/NotFound.tsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-  return (
-    <div className="notFpund">
-      <h1>Страница не найдена</h1>
-    </div>
-  );
-};
-
-export default NotFound;
--- src/types/combination.ts
+++
@@ -1,8 +0,0 @@
-export type CombinationType = {
-    combinationId: number;
-    name: string;
-    tag: string[];
-
-  };
-  
-//   {"combinationId":1,"name":"Cherry Turnover","tag":["Very Cherry","+","Very Cherry","+","Coconut","+","Buttered Popcorn"]}
